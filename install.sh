00dbd61aa5b62dc924b048ccc9149d0743127bf5
Big0x44 <58442405+Big0x44@users.noreply.github.com>
2024-11-14 11:43:24 -0500

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
-sudo apt install stow zsh git
+sudo apt install stow zsh git curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoupdate
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "Dependencies installed!"
+
+echo "Remove default zsh config"
+rm ~/.zshrc
 
 echo "Configure symlinks"
 stow .
